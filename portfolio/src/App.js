39e67157268e4179d5697ad6a977692742636ea8
Kimberly Boyd <kimberlyboyd@Kimberlys-MacBook-Pro.local>
2020-05-18 15:22:33 -0400

    hello done

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header";
-import "./components/Header.css"
+import Body from "./components/Body";
+import "./components/Header.css";
+import "./components/Body.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Body />
     </div>
   );
 }
---
+++ portfolio/src/components/Body.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Body.css"
+
+export default function Body() {
+    return (
+        <div className="body">
+            <div className="hello">
+                <h2>Hello! I'm Kim, nice to meet you.</h2>
+                <p>I am currently a student at Lambda School studying
+                Full Stack Web Development</p>
+                <div className="chat">
+                <p>Let's chat</p>
+                <a>kimberlyboyd2@gmail.com</a>
+                </div>
+            </div>
+        </div>
+    )
+}
